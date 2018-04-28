56ec576b12af1f5acd86a3c7b415b3cabefb7fdb
Leela-Prasad <leela.jagu@gmail.com>
2018-04-28 13:15:28 +0530

    Added Hystrix

--- src/main/java/com/microservices/limits/service/controllers/LimitsController.java
+++ src/main/java/com/microservices/limits/service/controllers/LimitsController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microservices.limits.service.configuration.LimitsServiceConfig;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsController {
@@ -15,6 +16,16 @@ public class LimitsController {
 	@GetMapping("/limits")
 	public String getLimits() {
 		return "Minimum : " + config.getMinimum() + "\t Maximum : " + config.getMaximum();
+	}
+	
+	@GetMapping("/limits2")
+	@HystrixCommand(fallbackMethod="fallbackLimits")
+	public String getLimits2() {
+		throw new RuntimeException("Exception occured iun Limits2 service");
+	}
+	
+	public String fallbackLimits() {
+		return "Minimum : " + Integer.MIN_VALUE + "\t Maximum : " + Integer.MAX_VALUE;
 	}
 	
 }
